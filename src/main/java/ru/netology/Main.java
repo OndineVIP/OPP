e42f38f7a5d8bd8b9ec23ca002a2ba1f547d6ea6
Ondine <ondinevika@gmail.com>
2023-05-08 13:57:15 +0300

    Add test

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.surname = "Иванов";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.passport = "4444 № 44444444";
+        post.subscription = "true";
+
+
+        post.birthday = new FormDate();
+        post.day = "13";
+        post.month = "6";
+        post.year = "1999";
+    }
+
+    {
+
+    }
+}
+
+
+
+
+
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+public class Post {
+    FormDate birthday = new FormDate();
+
+        public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public String subscription;
+
+    public String day;
+    public String month;
+    public String year;
+
+
+
+
+
+}
