58dd28c093c270817452599001fe740395b9315b
Ondine <ondinevika@gmail.com>
2023-05-22 11:31:41 +0300

    reformat code

--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -3,19 +3,16 @@ package ru.netology;
 public class Post {
     FormDate birthday = new FormDate();
 
-        public String name;
+    public String name;
     public String surname;
     public String patronymic;
     public String phone;
     public String passport;
-    public Boolean subscription;
+    public boolean subscription;
 
     public String day;
     public String month;
     public String year;
-
-
-
 
 
 }
